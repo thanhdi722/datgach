68368d5a8f457616d21a95dc994212b5ce407b27
thanhdi721 <thanhdi721@gmail.com>
2024-09-18 11:59:50 +0700

    updateeee

--- src/Component/PageIPhone16/FromInfo/form.main.tsx
+++ src/Component/PageIPhone16/FromInfo/form.main.tsx
@@ -993,6 +993,7 @@ export default function FormMain() {
                     <input
                       type="tel"
                       value={phone}
+                      className="input-formMB"
                       onChange={(e) => {
                         const re = /^[0-9\b]+$/;
                         if (e.target.value === "" || re.test(e.target.value)) {
