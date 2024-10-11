224cea7e21fd56dc9ad18bd7fae1db950b69f4f3
thanhdi721 <thanhdi721@gmail.com>
2024-10-11 15:03:29 +0700

    update title

--- src/Component/OldAutumnComponent/CardProductOldAutumn/CardProductOldAutumn.tsx
+++ src/Component/OldAutumnComponent/CardProductOldAutumn/CardProductOldAutumn.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import './CardProduct.css';
-import iconconhang from '../../../public/ic-shipped.png';
-import Image from 'next/image';
+
 export interface Product {
 	name: string;
 	image: string;
@@ -18,7 +17,7 @@ function CardProduct({ name, image, price }: Omit<Product, 'id'>) {
 				{name}
 			</div>
 			<div className='product__groupPrice' style={{ textAlign: 'center' }}>
-				<span className='product__price'>Giá thu: </span>
+				<span className='product__price'>Thu cao nhất: </span>
 				<span className='product__priceSpecial'>{price.toLocaleString()} VND</span>
 			</div>
 		</div>
