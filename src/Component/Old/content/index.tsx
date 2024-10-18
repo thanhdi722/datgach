3b6e3905fcb2f199a4402d6872f48f00e52be00f
CarsonDev1 <buitritinht@gmail.com>
2024-10-18 16:47:11 +0700

    upload content

--- src/Component/Old/content/index.tsx
+++ src/Component/Old/content/index.tsx
@@ -1,78 +1,73 @@
-import React, { useState } from "react";
-import Image from "next/image";
-import ContentImage from "../../../../public/old/content-01.jpg";
-import "./content.scss";
+import React, { useState } from 'react';
+import Image from 'next/image';
+import ContentImage from '../../../../public/old/content-01.jpg';
+import './content.scss';
+import { useQuery } from '@tanstack/react-query';
+
+const query = `
+query getCategories($filters:CategoryFilterInput){categories(filters:$filters){__typename items{description icon_image image_banner name url_path daily_sale{start_date end_date sale_type}slider_banner_left{title identifier Banner{items{banner_id caption link media media_alt name}page_info{current_page page_size total_pages}}}slider_banner_right{title identifier Banner{items{banner_id caption link media media_alt name}page_info{current_page page_size total_pages}}}daily_sale_id slider_id slider_two is_trend content_hot content_new check_show_category_in_page check_show_brand_in_page show children{breadcrumbs{...BreadcrumbFields}...CategoryTreeFields children{breadcrumbs{...BreadcrumbFields}...CategoryTreeFields children{breadcrumbs{...BreadcrumbFields}...CategoryTreeFields children{breadcrumbs{...BreadcrumbFields}...CategoryTreeFields}}}image_banner icon_image}}}}fragment CategoryTreeFields on CategoryTree{is_show_category_slider category_trend{__typename name image url_key url_path}icon_image image_banner slider_banner_left{title identifier Banner{items{banner_id caption link media media_alt name}page_info{current_page page_size total_pages}}}slider_banner_right{title identifier Banner{items{banner_id caption link media media_alt name}page_info{current_page page_size total_pages}}}slider_two is_trend show check_show_category_in_page check_show_brand_in_page content_hot content_new uid id available_sort_by canonical_url name image include_in_menu meta_description meta_keywords meta_title display_mode url_key url_path description path path_in_store children_count position}fragment BreadcrumbFields on Breadcrumb{category_level category_name category_uid category_url_key category_url_path}
+`;
+
+const variables = {
+	filters: { url_key: { eq: 'product-old' } },
+};
+
+async function fetchContentData() {
+	const response = await fetch('https://beta-api.bachlongmobile.com/graphql', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			query,
+			variables,
+		}),
+	});
+
+	const data = await response.json();
+	return data.data.categories.items;
+}
 
 const Content = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
+	const [isExpanded, setIsExpanded] = useState(false);
 
-  const toggleExpand = () => {
-    setIsExpanded(!isExpanded);
-  };
+	const { data, error, isLoading } = useQuery({
+		queryKey: ['contentData'],
+		queryFn: fetchContentData,
+		staleTime: 300000,
+	});
 
-  return (
-    <div className="content">
-      <div className="container">
-        <div className="content-wrap">
-          <div
-            className={`content-item ${isExpanded ? "expanded" : "collapsed"}`}
-          >
-            Có nên <span style={{ fontWeight: 600 }}>mua điện thoại cũ</span>{" "}
-            nói riêng và hàng công nghệ cũ nói chung hay không là thắc mắc của
-            không ít người tiêu dùng. Trên thực tế việc mua hàng công nghệ đã
-            qua sử dụng mang đến nhiều lợi ích nhưng cũng không hiếm các rủi ro.
-            Để đưa ra quyết định chọn mua máy cũ mời bạn tham khảo qua bài viết
-            bên dưới.{" "}
-            <q>Vì sao nhiều người chọn mua điện thoại cũ thay vì hàng mới?</q>{" "}
-            Lý do đầu tiên chắc hẳn bạn cũng đã mường tượng được trong đầu chính
-            là mua máy cũ giá rẻ để tiết kiệm chi phí. Thay vì phải bỏ ra một số
-            tiền lớn để mua máy mới, nếu mua hàng cũ thì số tiền ấy sẽ thấp hơn
-            khoảng 10 - 50% tùy máy. Dù bỏ ra chi phí thấp hơn nhưng người dùng
-            vẫn được trải nghiệm tính năng cao cấp, mức cấu hình mạnh tương
-            đương máy mới. Thứ hai một số bạn yêu thích đồ công nghệ và luôn
-            muốn trải nghiệm nhiều máy khác nhau nhưng lại có khả năng tài chính
-            hạn hẹp, vì thế họ tìm đến đồ công nghệ cũ. Với cách này các bạn ấy
-            vừa được thỏa mãn nhu cầu đổi điện thoại, vừa có thể tiết kiệm chi
-            phí. Cuối cùng có những dòng máy đã không còn được hãng sản xuất
-            mới, nhưng có nhiều bạn lại yêu thích một tính năng hay thiết kế nào
-            đó trên máy, vì thế họ tìm đến đồ cũ. Tương tự như thế, nhiều khách
-            hàng có sở thích sưu tập những chiếc smartphone cổ, vì thế những
-            quán bán đồ cũ hay tiệm cầm đồ là nơi họ chọn mua thiết bị công
-            nghệ.{" "}
-            <q>Khi mua máy đổi trả, cũ đã qua sử dụng cần quan tâm những gì?</q>{" "}
-            Bên cạnh những lợi ích tuyệt vời, việc chọn mua máy cũ cũng tồn tại
-            không ít rủi ro như ngoại hình, lỗi phần mềm, máy đã qua sửa
-            chữa,.... Do đó để chọn được sản phẩm ưng ý, bạn cần lưu tâm đến
-            những điều dưới đây: <q>Nơi mua uy tín</q> Chọn được địa chỉ mua uy
-            tín sẽ đảm bảo chất lượng của máy. Theo đó những máy cũ được thu mua
-            lại ở những địa điểm này sẽ được kiểm tra kỹ lưỡng trước khi được
-            bán lại cho người tiêu dùng. Để chọn được nơi mua uy tín, bạn cần
-            tham khảo ý kiến của những khách hàng đã từng mua hàng tại đó. Hoặc
-            bạn có thể lên mạng tìm hiểu về mức độ uy tín của địa chỉ đó như thế
-            nào, đã từng bị khách hàng đánh giá không tốt hay chưa,...
-          </div>
-          {isExpanded && (
-            <div className="image-wrap">
-              <Image
-                src={ContentImage}
-                alt="content"
-                width={1000}
-                height={1000}
-              />
-            </div>
-          )}
-        </div>
+	const contentData = data?.[0]?.description;
 
-        <div style={{ marginTop: "10px", textAlign: "center" }}>
-          <button className="button" onClick={toggleExpand}>
-            <span className="button-content">
-              {isExpanded ? "Thu gọn" : "Xem thêm"}
-            </span>
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+	console.log('contentData: ', contentData);
+
+	const toggleExpand = () => {
+		setIsExpanded(!isExpanded);
+	};
+
+	return (
+		<div className='content'>
+			<div className='container'>
+				<div className='content-wrap'>
+					<div
+						className={`content-item ${isExpanded ? 'expanded' : 'collapsed'}`}
+						dangerouslySetInnerHTML={{ __html: contentData }}
+					></div>
+					{isExpanded && (
+						<div className='image-wrap'>
+							<Image src={ContentImage} alt='content' width={1000} height={1000} />
+						</div>
+					)}
+				</div>
+
+				<div style={{ marginTop: '10px', textAlign: 'center' }}>
+					<button className='button' onClick={toggleExpand}>
+						<span className='button-content'>{isExpanded ? 'Thu gọn' : 'Xem thêm'}</span>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Content;
