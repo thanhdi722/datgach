5431038aa40f6458ad653a0f64afa037b9650291
CarsonDev1 <buitritinht@gmail.com>
2024-10-28 16:48:18 +0700

    upload code

--- src/Component/ComponentPageHalloween/accessories20_10/acess-210/index.tsx
+++ src/Component/ComponentPageHalloween/accessories20_10/acess-210/index.tsx
@@ -4,7 +4,10 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Spin } from 'antd';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import DecorProduct from '../../../../../public/halloween/ic-to.png';
 import DecorProduct2 from '../../../../../public/halloween/ICON-DRAGON.png';
 import FrameProduct from '../../../../../public/halloween/frame-product.png';
@@ -56,9 +59,15 @@ const Access210: React.FC = () => {
 					<Spin size='large' />
 				</div>
 			) : (
-				<div className='upgrade'>
+				<div className=''>
 					{productSale.length > 5 ? (
-						<Swiper spaceBetween={10} slidesPerView={3} navigation pagination={{ clickable: true }}>
+						<Swiper
+							modules={[Navigation, Pagination]}
+							spaceBetween={10}
+							slidesPerView={5}
+							navigation
+							pagination={{ clickable: true }}
+						>
 							{productSale.map((item: any, index: number) => (
 								<SwiperSlide key={index}>
 									<Link
@@ -159,94 +168,102 @@ const Access210: React.FC = () => {
 							))}
 						</Swiper>
 					) : (
-						productSale.map((item: any, index: number) => (
-							<Link
-								key={index}
-								href={`https://bachlongmobile.com/products/${item.product?.url_key}`}
-								passHref
-								target='_blank'
-								rel='noopener noreferrer'
-								style={{ textDecoration: 'none', color: 'black' }}
-							>
-								<div className='upgrade-item'>
-									<div className='upgrade-item-header'>
-										<Image
-											src={DecorProduct}
-											width={80}
-											height={80}
-											quality={100}
-											alt='decor-product'
-											className='decor-product'
-										/>
-										<Image
-											src={DecorProduct2}
-											width={80}
-											height={80}
-											quality={100}
-											alt='decor-product'
-											className='decor-product2'
-										/>
-										<span></span>
-										<span className='percent'>Trả góp 0%</span>
-									</div>
-									<div className='upgrade-item-img'>
-										<div className='img-content'>
+						<div className='upgrade'>
+							{productSale.map((item: any, index: number) => (
+								<Link
+									key={index}
+									href={`https://bachlongmobile.com/products/${item.product?.url_key}`}
+									passHref
+									target='_blank'
+									rel='noopener noreferrer'
+									style={{ textDecoration: 'none', color: 'black' }}
+								>
+									<div className='upgrade-item'>
+										<div className='upgrade-item-header'>
 											<Image
-												src={item.product?.image.url}
-												width={1400}
-												height={1200}
+												src={DecorProduct}
+												width={80}
+												height={80}
 												quality={100}
-												alt={`product-${index}`}
+												alt='decor-product'
+												className='decor-product'
 											/>
-										</div>
-										<div className='frame-product'>
 											<Image
-												src={FrameProduct}
-												width={500}
-												height={500}
+												src={DecorProduct2}
+												width={80}
+												height={80}
 												quality={100}
-												alt='frame-product'
+												alt='decor-product'
+												className='decor-product2'
 											/>
+											<span></span>
+											<span className='percent'>Trả góp 0%</span>
 										</div>
-									</div>
-									<div className='upgrade-item-content'>
-										<h4 className='upgrade-item-content-tt'>{item.product.name}</h4>
-										<div className='upgrade-item-content-body'>
-											<div className='upgrade-item-content-body-price'>
-												{currentDate >= targetDate ? (
-													<>
-														{Number(item?.sale_price)?.toLocaleString('vi-VN')}{' '}
-														{item?.product.price_range.minimum_price.final_price.currency}
-													</>
-												) : (
-													<>
-														{formatPriceWithCondition(Number(item?.sale_price))}{' '}
-														{item?.product.price_range.minimum_price.final_price.currency}
-													</>
-												)}
+										<div className='upgrade-item-img'>
+											<div className='img-content'>
+												<Image
+													src={item.product?.image.url}
+													width={1400}
+													height={1200}
+													quality={100}
+													alt={`product-${index}`}
+												/>
 											</div>
-											<div className='upgrade-item-content-body-reduced'>
-												<div className='price-reduced'>
-													{Number(item?.price_original)?.toLocaleString('vi-VN')}{' '}
-													{item?.product.price_range.minimum_price.final_price.currency}
+											<div className='frame-product'>
+												<Image
+													src={FrameProduct}
+													width={500}
+													height={500}
+													quality={100}
+													alt='frame-product'
+												/>
+											</div>
+										</div>
+										<div className='upgrade-item-content'>
+											<h4 className='upgrade-item-content-tt'>{item.product.name}</h4>
+											<div className='upgrade-item-content-body'>
+												<div className='upgrade-item-content-body-price'>
+													{currentDate >= targetDate ? (
+														<>
+															{Number(item?.sale_price)?.toLocaleString('vi-VN')}{' '}
+															{
+																item?.product.price_range.minimum_price.final_price
+																	.currency
+															}
+														</>
+													) : (
+														<>
+															{formatPriceWithCondition(Number(item?.sale_price))}{' '}
+															{
+																item?.product.price_range.minimum_price.final_price
+																	.currency
+															}
+														</>
+													)}
 												</div>
-												{currentDate >= targetDate && (
-													<div className='percent'>
-														-
-														{Math.ceil(
-															((item?.price_original - item.sale_price) /
-																item?.price_original) *
-																100
-														)}
-														%
+												<div className='upgrade-item-content-body-reduced'>
+													<div className='price-reduced'>
+														{Number(item?.price_original)?.toLocaleString('vi-VN')}{' '}
+														{item?.product.price_range.minimum_price.final_price.currency}
 													</div>
-												)}
+													{currentDate >= targetDate && (
+														<div className='percent'>
+															-
+															{Math.ceil(
+																((item?.price_original - item.sale_price) /
+																	item?.price_original) *
+																	100
+															)}
+															%
+														</div>
+													)}
+												</div>
 											</div>
 										</div>
 									</div>
-								</div>
-							</Link>
-						))
+								</Link>
+							))}
+						</div>
 					)}
 				</div>
 			)}
