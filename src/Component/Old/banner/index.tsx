9a6faff9e7611b97fbee31c293e25af198b40e3a
CarsonDev1 <buitritinht@gmail.com>
2024-11-04 09:18:47 +0700

    upload code

--- src/Component/Old/banner/index.tsx
+++ src/Component/Old/banner/index.tsx
@@ -1,22 +1,115 @@
 'use client';
 import React from 'react';
 import Image from 'next/image';
+import { Spin } from 'antd';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
 import './banner.scss';
-import bannerDesktop from '../../../../public/old/banner-apple.jpg';
 import bannerMobile from '../../../../public/old/banner-mobile.jpg';
+import { useQuery } from '@tanstack/react-query';
+
+const query = `
+query getSlider($filter: SliderFilterInput) {
+  Slider(filter: $filter) {
+    items {
+      title
+      identifier
+      Banner {
+        __typename
+        items {
+          banner_id
+          caption
+          link
+          media
+          media_alt
+          name
+          slider_id
+        }
+        page_info {
+          current_page
+          page_size
+          total_pages
+        }
+      }
+    }
+    total_count
+  }
+}`;
+
+const variables = {
+	filter: {
+		identifier: {
+			eq: 'banner-may-cu',
+		},
+	},
+};
+
+async function fetchBannerData() {
+	const response = await fetch('https://beta-api.bachlongmobile.com/graphql', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			query,
+			variables,
+		}),
+	});
+
+	const data = await response.json();
+	return data.data.Slider.items;
+}
 
 const Banner: React.FC = () => {
+	const { data, isLoading } = useQuery({
+		queryKey: ['fetchBannerData'],
+		queryFn: fetchBannerData,
+		staleTime: 300000,
+	});
+
+	const categoryData = data?.[0]?.Banner?.items;
+	const bannerDesktopData = categoryData?.find((cateData: { name: string }) =>
+		cateData.name.startsWith('Head Máy Cũ Desktop')
+	);
+	const bannerMobileData = categoryData?.find((cateData: { name: string }) =>
+		cateData.name.startsWith('Head Máy Cũ Mobile')
+	);
+
 	return (
 		<div className='banner'>
 			<div className='banner-desktop'>
-				<Image src={bannerDesktop} alt='banner desktop' width={1820} height={1400} />
+				{isLoading ? (
+					<Spin>
+						<div style={{ width: 1820, height: 500 }} />
+					</Spin>
+				) : (
+					bannerDesktopData && (
+						<Image
+							src={bannerDesktopData.media}
+							alt={bannerDesktopData.media_alt || 'banner desktop'}
+							width={1820}
+							height={1400}
+						/>
+					)
+				)}
 			</div>
 			<div className='banner-mobile'>
-				<Image src={bannerMobile} alt='banner mobile' width={1820} height={1820} />
+				{isLoading ? (
+					<Spin>
+						<div style={{ width: 1820, height: 500 }} />
+					</Spin>
+				) : (
+					bannerMobileData && (
+						<Image
+							src={bannerMobileData.media}
+							alt={bannerMobileData.media_alt || 'banner desktop'}
+							width={1820}
+							height={1400}
+						/>
+					)
+				)}
 			</div>
 		</div>
 	);
