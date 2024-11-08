8f66d6803409751753751a3cba470a6aea013153
DESKTOP-FECONH7\THANH DI <thanhdi721@gmail.com>
2024-11-08 09:22:08 +0700

    update banner xem-ky-han

--- src/Component/Xemkyhan/Header/BannerHeader.tsx
+++ src/Component/Xemkyhan/Header/BannerHeader.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../../style/styleheaderbanner.css";
 import banner1 from "../../../../public/Images/Gioi_thieu_BLM_1200x450_0810-3.jpg";
 import banner2 from "../../../../public/Images/BOX-TRAGOP-8-150624.png";
@@ -15,9 +15,102 @@ import banner8 from "../../../../public/Images/BOX-TRAGOP-4-150624.png";
 import banner9 from "../../../../public/Images/BOX-TRAGOP-5-150624 (1).png";
 import banner10 from "../../../../public/Images/040924-BANNER-TRA-GOP-3-KHONG-1200.png";
 import banner11 from "../../../../public/Images/banner11.png";
+interface BannerItem {
+  banner_id: number;
+  caption: string;
+  link: string;
+  media: string;
+  media_alt: string;
+  name: string;
+  slider_id: number;
+}
 
+interface Banner {
+  __typename: string;
+  items: BannerItem[];
+  page_info: {
+    current_page: number;
+    page_size: number;
+    total_pages: number;
+  };
+}
+
+interface SliderItem {
+  title: string;
+  identifier: string;
+  Banner: Banner;
+}
+
+interface SliderData {
+  Slider: {
+    items: SliderItem[];
+    total_count: number;
+  };
+}
+
+interface ApiResponse {
+  data: SliderData;
+}
 import Link from "next/link";
+import { Spin } from "antd";
 export default function BannerHeader() {
+  const [dataTitle, setDataTitle] = useState<ApiResponse | null>(null);
+  const fetchBannerHeader = async () => {
+    try {
+      const response = await fetch(
+        "https://beta-api.bachlongmobile.com/graphql",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            query: `
+                  query getSlider($filter: SliderFilterInput) {
+                    Slider(filter: $filter) {
+                      items {
+                        title
+                        identifier
+                        Banner {
+                          __typename
+                          items {
+                            banner_id
+                            caption
+                            link
+                            media
+                            media_alt
+                            name
+                            slider_id
+                          }
+                          page_info {
+                            current_page
+                            page_size
+                            total_pages
+                          }
+                        }
+                      }
+                      total_count
+                    }
+                  }
+                `,
+            variables: {
+              filter: {
+                identifier: {
+                  eq: "banner-xem-ky-han",
+                },
+              },
+            },
+          }),
+        }
+      );
+
+      const result = await response.json();
+      setDataTitle(result);
+    } catch (err) {}
+  };
+  useEffect(() => {
+    fetchBannerHeader();
+  }, []);
   return (
     <div
       style={{
@@ -59,16 +152,57 @@ export default function BannerHeader() {
           height={675}
         />
         <div className="wrapper-banner2">
-          <Link href={`#`}>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                // backgroundColor: "#fff",
-                gap: "10px",
-              }}
-            >
-              <Image
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              // backgroundColor: "#fff",
+              gap: "10px",
+            }}
+          >
+            {dataTitle ? (
+              dataTitle?.data?.Slider?.items[0]?.Banner?.items
+                .filter((item) => item.name.includes("banner xem kỳ hạn 1"))
+                .map((item, index) => (
+                  <div key={index}>
+                    <Link href={item?.link} target="_blank">
+                      <Image
+                        width={1000}
+                        height={1000}
+                        className="bannerline1"
+                        src={item.media || ""}
+                        alt={`privilege-${index + 1}`}
+                      />
+                    </Link>
+                  </div>
+                ))
+            ) : (
+              <Spin>
+                <div style={{ width: 200, height: 200 }} />
+              </Spin>
+            )}
+            {dataTitle ? (
+              dataTitle?.data?.Slider?.items[0]?.Banner?.items
+                .filter((item) => item.name.includes("banner xem kỳ hạn 2"))
+                .map((item, index) => (
+                  <div key={index}>
+                    <Link href={item?.link} target="_blank">
+                      <Image
+                        width={1000}
+                        height={1000}
+                        className="bannerline1"
+                        src={item.media || ""}
+                        alt={`privilege-${index + 1}`}
+                      />
+                    </Link>
+                  </div>
+                ))
+            ) : (
+              <Spin>
+                <div style={{ width: 200, height: 200 }} />
+              </Spin>
+            )}
+            {/* <Image
                 src={banner4}
                 alt=""
                 width={1000}
@@ -81,59 +215,112 @@ export default function BannerHeader() {
                 width={1000}
                 height={1000}
                 className="bannerline1"
-              />
-            </div>
-          </Link>
-          <Link href={`#`}>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                // backgroundColor: "#fff",
-                gap: "10px",
-              }}
-            >
-              <Image
-                src={banner6}
-                className="bannerline2"
-                alt=""
-                width={1000}
-                height={1000}
-              />
-              <Image
-                src={banner7}
-                alt=""
-                width={1000}
-                height={1000}
-                className="bannerline2"
-              />
-            </div>
-          </Link>
-          <Link href={`#`}>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                // backgroundColor: "#fff",
-                gap: "10px",
-              }}
-            >
-              <Image
-                src={banner12}
-                className="bannerline2"
-                alt=""
-                width={1000}
-                height={1000}
-              />
-              <Image
-                src={banner13}
-                alt=""
-                width={1000}
-                height={1000}
-                className="bannerline2"
-              />
-            </div>
-          </Link>
+              /> */}
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              // backgroundColor: "#fff",
+              gap: "10px",
+            }}
+          >
+            {dataTitle ? (
+              dataTitle?.data?.Slider?.items[0]?.Banner?.items
+                .filter((item) => item.name.includes("banner xem kỳ hạn 3"))
+                .map((item, index) => (
+                  <div key={index}>
+                    <Link href={item?.link} target="_blank">
+                      <Image
+                        className="bannerline2"
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        src={item.media || ""}
+                      />
+                    </Link>
+                  </div>
+                ))
+            ) : (
+              <Spin>
+                <div style={{ width: 200, height: 200 }} />
+              </Spin>
+            )}
+            {dataTitle ? (
+              dataTitle?.data?.Slider?.items[0]?.Banner?.items
+                .filter((item) => item.name.includes("banner xem kỳ hạn 4"))
+                .map((item, index) => (
+                  <div key={index}>
+                    <Link href={item?.link} target="_blank">
+                      <Image
+                        className="bannerline2"
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        src={item.media || ""}
+                      />
+                    </Link>
+                  </div>
+                ))
+            ) : (
+              <Spin>
+                <div style={{ width: 200, height: 200 }} />
+              </Spin>
+            )}
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              // backgroundColor: "#fff",
+              gap: "10px",
+            }}
+          >
+            {dataTitle ? (
+              dataTitle?.data?.Slider?.items[0]?.Banner?.items
+                .filter((item) => item.name.includes("banner xem kỳ hạn 5"))
+                .map((item, index) => (
+                  <div key={index}>
+                    <Link href={item?.link} target="_blank">
+                      <Image
+                        className="bannerline2"
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        src={item.media || ""}
+                      />
+                    </Link>
+                  </div>
+                ))
+            ) : (
+              <Spin>
+                <div style={{ width: 200, height: 200 }} />
+              </Spin>
+            )}
+            {dataTitle ? (
+              dataTitle?.data?.Slider?.items[0]?.Banner?.items
+                .filter((item) => item.name.includes("banner xem kỳ hạn 6"))
+                .map((item, index) => (
+                  <div key={index}>
+                    <Link href={item?.link} target="_blank">
+                      <Image
+                        className="bannerline2"
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        src={item.media || ""}
+                      />
+                    </Link>
+                  </div>
+                ))
+            ) : (
+              <Spin>
+                <div style={{ width: 200, height: 200 }} />
+              </Spin>
+            )}
+          </div>
         </div>
 
         <Image
