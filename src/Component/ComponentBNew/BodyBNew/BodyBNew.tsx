f3230c6c85f05185b877b0105beb3a543669ee84
DESKTOP-FECONH7\THANH DI <thanhdi721@gmail.com>
2024-11-13 16:54:44 +0700

    upadte news

--- src/Component/ComponentBNew/BodyBNew/BodyBNew.tsx
+++ src/Component/ComponentBNew/BodyBNew/BodyBNew.tsx
@@ -51,9 +51,9 @@ export default function BodyBNew() {
     allPosts: false,
     sort: ["DESC"],
   };
-  const variablesProduct = {
+  const variablesProductKM = {
     filter: { category_id: { eq: 22 } },
-    pageSize: 1000,
+    pageSize: 100,
     currentPage: 1,
     allPosts: false,
     sort: ["DESC"],
@@ -103,7 +103,7 @@ export default function BodyBNew() {
         },
         body: JSON.stringify({
           query: `query blogPosts( $filter: BlogPostsFilterInput $pageSize: Int $currentPage: Int $sortFiled: String $allPosts: Boolean $sort: [String] ) { blogPosts( filter: $filter pageSize: $pageSize currentPage: $currentPage sortFiled: $sortFiled allPosts: $allPosts sort: $sort ) { items { author { author_id author_url content creation_time custom_theme_to facebook_page_url featured_image filtered_content identifier instagram_page_url is_active layout_update_xml linkedin_page_url meta_description meta_title name page_layout relative_url title twitter_page_url type url } author_id canonical_url category_id content_heading creation_time end_time featured_image featured_img_alt featured_list_image featured_list_img_alt first_image identifier is_active page_layout position post_id post_url publish_time search title type update_time views_count categories { canonical_url category_id category_level category_url category_url_path content content_heading custom_layout custom_layout_update_xml custom_theme custom_theme_from custom_theme_to display_mode featured_img identifier include_in_menu is_active layout_update_xml meta_description meta_keywords meta_title page_layout parent_category_id path position posts_count posts_sort_by relative_url title type breadcrumbs { category_id category_level category_name category_uid category_url_key category_url_path } } filtered_content media_gallery { url } meta_description meta_keywords meta_title promotion_image tags { content custom_layout custom_layout_update_xml custom_theme custom_theme_from custom_theme_to identifier is_active layout_update_xml meta_description meta_keywords meta_robots meta_title page_layout relative_url tag_id tag_url title type } tag_id short_content short_filtered_content } total_count total_pages type } }`,
-          variablesProduct,
+          variables: variablesProductKM,
         }),
       }
     );
@@ -139,6 +139,7 @@ export default function BodyBNew() {
     fetchBlogPostsDataNew(); // Fetch new posts based on activeTab2
     fetchBlogPostsDatass(); // Fetch new posts based on activeTab2
   }, [activeTab, activeTab2]);
+
   const tabs = [
     "Trang Chủ",
     "Tin Công Nghệ",
